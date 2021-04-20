1c75b9e72110e69f38e1819b19f3ab6ab67d8fcc
Richard Awoyemi <Richardawoyemi@gmail.com>
2021-04-20 17:45:10 +0100

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGWFMDwK51OU3FqfZ1XF11Wf_-P5SYv2k",
+    authDomain: "ecomi-community.firebaseapp.com",
+    projectId: "ecomi-community",
+    storageBucket: "ecomi-community.appspot.com",
+    messagingSenderId: "158709385260",
+    appId: "1:158709385260:web:684a87e1cb9274a353e989",
+    measurementId: "G-GVM90YYPXJ"
+  }
 };
