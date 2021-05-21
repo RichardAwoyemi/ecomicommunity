a0acb9f20844d852f67deec1a18e3450b34549a8
Richard Awoyemi <Richardawoyemi@gmail.com>
2021-05-21 21:39:48 +0100

    Function Utils

--- functions/src/utils/function.utils.ts
+++ functions/src/utils/function.utils.ts
@@ -12,19 +12,67 @@ export interface ITransactionSummary {
   selling: IAmount
 }
 
+
 export interface IAmount {
-  userEmail: string;
-  username: string;
-  currency: string;
-  units: number;
-  networkSymbol: string,
-  walletAddress: string;
+  useruid?: string;
+  username?: string;
+  itemType?: AppTransactionItemTypes;
+  currency?: AppTransactionCurrencies;
+  units?: number;
+  sendingWallet?: IWallet;
+  receivingWallet?: IWallet;
+  fees?: IFees;
+}
+
+export interface IWallet {
+  network?: Networks,
+  networkSymbol?: NetworkSymbols,
+  walletAddress?: string;
   veveUsername?: string;
-  fees: IFees;
 }
 
 export interface IFees {
   networkFees: number;
   platformFees: number;
   totalPostFees: number;
+}
+
+export enum AppTransactionItemTypes {
+  Collectible = 'Collectible',
+  Currency = 'Currency',
+}
+
+export enum AppTransactionCurrencies {
+  GEMS = 'GEMS',
+  BTC = 'BTC',
+  BNB = 'BNB',
+  DAI = 'DAI',
+  OMI = 'OMI',
+  USDC = 'USDC',
+  USDT = 'USDT',
+}
+
+export enum Networks {
+  ERC20 = 'Ethereum Network (ERC20)',
+  BEP20 = 'Binance Smart Chain (BEP20)',
+  BTC = 'Bitcoin (BTC)',
+  GO = 'GoChain (GO)',
+  VEVE = 'VeVe App',
+  NULL = 'NULL'
+}
+
+export enum NetworkSymbols {
+  ERC20 = 'ERC20',
+  BEP20 = 'BEP20',
+  BTC = 'BTC',
+  GO = 'GO',
+  VEVE = 'VEVE'
+}
+
+export enum NetworkAddresses {
+  ERC20 = '0xA6B300A0F83a3D49b720F1B5734Fee4A59AEddce',
+  BEP20 = '0xA6B300A0F83a3D49b720F1B5734Fee4A59AEddce',
+  BTC = '',
+  GO = '',
+  VEVE = '',
 }
